3bd34609778ce7cd7afb8836aa84f9786909cf7e
rogersanick <rogers.a.nick@gmail.com>
2018-09-04 19:38:44 -0400

    WIP bug fix. Current provider not being correctly set for instance of web3. Has something to do with address being returned from Infura. More investigation is required.:w

--- ethereum/web3.js
+++ ethereum/web3.js
@@ -1,5 +1,4 @@
 import Web3 from 'web3';
-require('dotenv').config();
 
 let web3;
 
@@ -8,13 +7,11 @@ if (typeof window !== 'undefined' && typeof window.web3 !== 'undefined') {
   web3 = new Web3(window.web3.currentProvider);
 } else {
   // We are on the server *OR* the user is not running metamask
-
-  const currentProvider = new web3.providers.HttpProvider(
-    'https://rinkeby.infura.io/v3/cff505b264bb479b9df907c80ee66f5f'
+  const currentProvider = new Web3.providers.WebsocketProvider(
+    'wss://rinkeby.infura.io/v3/cff505b264bb479b9df907c80ee66f5f'
   );
 
-  web3 = new Web3(provider);
-
+  web3 = new Web3(currentProvider);
 }
 
 export default web3;
