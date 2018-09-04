13b609c0e8212e827f6ce65c629ebc5d6909f046
rogersanick <rogers.a.nick@gmail.com>
2018-09-04 12:46:36 -0400

    Refactored deploy script to write address of latest deployed version of campaignFactory to seperate js file in parent directory.

--- ethereum/deploy.js
+++ ethereum/deploy.js
@@ -2,6 +2,8 @@ const HDWalletProvider = require('truffle-hdwallet-provider');
 const Web3 = require('web3');
 require('dotenv').config();
 const compiledFactory = require('./build/campaignFactory.json')
+const fs = require('fs-extra');
+const path = require('path');
 
 const provider = new HDWalletProvider(
   process.env.mnemonic,
@@ -9,6 +11,8 @@ const provider = new HDWalletProvider(
 );
 
 const web3 = new Web3(provider);
+
+const deployedAddressPath = path.resolve(__dirname, '../address.js');
 
 (async () => {
   const accounts = await web3.eth.getAccounts();
@@ -18,6 +22,10 @@ const web3 = new Web3(provider);
     .deploy({ data: '0x' + compiledFactory.bytecode })
     .send({ gas: '1000000', from: accounts[0] });
 
-  console.log('Contract deployed to ', result.options.address);
+
+  console.log(result.options.address);
+  fs.removeSync(deployedAddressPath);
+  fs.writeFileSync(deployedAddressPath , `module.exports = ${result.options.address};`);
+  console.log('successfully deployed, contract address now accessible in address.js');
 
 })();
