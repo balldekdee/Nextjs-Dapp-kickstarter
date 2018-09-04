14db50d87c8c0179e99c1014b7516e465df957bf
rogersanick <rogers.a.nick@gmail.com>
2018-09-04 12:13:25 -0400

    Added end-to-end test for approving request to spend funds in kickstart campaign. Started work on deployment script.:w

---
+++ ethereum/deploy.js
@@ -0,0 +1,24 @@
+const HDWalletProvider = require('truffle-hdwallet-provider');
+const Web3 = require('web3');
+require('dotenv').config();
+const { interface, bytecode } = require('./compile.js');
+
+const provider = new HDWalletProvider(
+  process.env.mnemonic,
+  'https://rinkeby.infura.io/v3/cff505b264bb479b9df907c80ee66f5f'
+);
+
+const web3 = new Web3(provider);
+
+(async () => {
+  const accounts = await web3.eth.getAccounts();
+  console.log('Attempting to deploy from account', accounts[0]);
+
+  const results = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({ data: '0x' + bytecode })
+    .send({ gas: '1000000', from: accounts[0] });
+
+  console.log(interface);
+  console.log('Contract deployed to ', result.options.address);
+
+})
--- test/Campaign.test.js
+++ test/Campaign.test.js
@@ -54,6 +54,82 @@ describe('Campaigns', () => {
 
     assert(campaign.methods.approvers(accounts[1]).call());
 
-  })
+  });
 
-})
+  it('requires a minimum contribution for a given campaign', async () => {
+    try {
+      await campaign.methods.contribute().send({
+        from: accounts[2],
+        value: web3.utils.toWei('0.9', 'ether'),
+        gas: '1000000'
+      });
+      assert(false);
+    } catch (err) {
+      assert(true);
+    }
+  });
+
+  it('allows a manager to make a payment request', async () => {
+    await campaign.methods.makeRequest("test request", web3.utils.toWei('1','ether'), accounts[1]).send({
+      from: accounts[1],
+      gas: '1000000'
+    });
+    const {description, value, recipient, complete, approvalCount} = await campaign.methods.requests(0).call();
+    assert.equal(description, "test request");
+    assert.equal(value, Web3.utils.toWei('1','ether'));
+    assert.equal(recipient, accounts[1]);
+    assert.equal(complete, false);
+    assert.equal(approvalCount, 0);
+  });
+
+  it('processes requests', async () => {
+
+    await campaign.methods.contribute().send({
+      from: accounts[0],
+      value: web3.utils.toWei('1.1','ether'),
+      gas: '1000000'
+    });
+
+    await campaign.methods.contribute().send({
+      from: accounts[1],
+      value: web3.utils.toWei('1.1','ether'),
+      gas: '1000000'
+    });
+
+    const requesterBalance = await web3.eth.getBalance(accounts[1]); 
+
+    await campaign.methods.makeRequest("Test Request 2", web3.utils.toWei('2', 'ether'), accounts[1]).send({
+      from: accounts[0],
+      gas:'1000000'
+    });
+
+    await campaign.methods.approveRequest(0).send({
+      from: accounts[1],
+      gas: '1000000'
+    });
+
+    try {
+      await campaign.methods.finalizeRequest(0).send({
+        from: accounts[0],
+        gas: '1000000'
+      });
+      assert(false);
+    } catch (err) {
+      assert(true);
+    }
+
+    await campaign.methods.approveRequest(0).send({
+      from: accounts[0],
+      gas: '1000000'
+    });
+
+    await campaign.methods.finalizeRequest(0).send({
+      from: accounts[0],
+      gas:'1000000'
+    });
+
+    assert(await web3.eth.getBalance(accounts[1]) > requesterBalance);
+
+  });
+
+});
