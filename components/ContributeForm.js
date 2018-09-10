c06a1bdbffa1df2f69b2b52bbed94154a91d3468
rogersanick <rogers.a.nick@gmail.com>
2018-09-10 15:48:22 -0400

    Built MVP campaign detail view with information cards for address of manager, minimum contribution, number of approvers & campaign balance.

---
+++ components/ContributeForm.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Form, Input, Message } from 'semantic-ui-react';
+
+class ContributeForm extends React.component {
+  render() {
+    <Form>
+
+    </Form>
+  };
+}
+
+export default ContributeForm;
+
--- pages/campaigns/show.js
+++ pages/campaigns/show.js
@@ -1,17 +1,75 @@
 import React from 'react';
 import Layout from '../../components/Layout.js';
-
+import Campaign from '../../ethereum/campaign.js';
+import { Card } from 'semantic-ui-react';
+import web3 from '../../ethereum/web3.js'
 class CampaignShow extends React.Component {
 
   static async getInitialProps(props) {
-    console.log(props.query.address);
-    return {};
+    const campaign = Campaign(props.query.address);
+    const summary = await campaign.methods.getSummary().call();
+    console.log(summary);
+    return { 
+      minimumContribution: summary[1],
+      balance: summary[1],
+      requestsCount: summary[2],
+      approversCount: summary[3],
+      manager: summary[4]
+    };
+  }
+
+  renderCards() {
+
+    const {
+      balance,
+      manager,
+      minimumContribution,
+      requestsCount,
+      approversCount
+    } = this.props;
+
+    const items = [
+      {
+        header: manager,
+        meta: 'Address of manager',
+        description: 'The manager created this campaign and can create requests to withdraw money.',
+        style: { overflowWrap: 'break-word'}
+      },
+      {
+        header: minimumContribution,
+        meta: 'minimumContribution (wei)',
+        description: 'You must contribute at least this much Wei to participate in the crowdfund.',
+        style: { overflowWrap: 'break-word'}
+      },
+      {
+        header: requestsCount,
+        meta: 'Number of Requests',
+        description: 'A request tries to withdraw money from the contract. Requests must be approved by approvers.',
+        style: { overflowWrap: 'break-word'}
+      },
+      {
+        header: approversCount,
+        meta: 'Number of Approvers',
+        description: 'Number of people who have already donated to this campaign',
+        style: { overflowWrap: 'break-word'}
+      },
+      {
+        header: web3.utils.fromWei(balance, 'ether'),
+        meta: 'Campaign Balance',
+        description: 'How much ETH individuals have donated to this campaign.',
+        style: { overflowWrap: 'break-word'}
+      }
+    ];
+
+    return < Card.Group items = {items} />
+
   }
 
   render() {
     return (
       <Layout>
         <h3>CampaignShow</h3>
+        {this.renderCards()}
       </Layout>
     );
   }
