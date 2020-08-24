5f818f5e43c19e866013930b79ab1a2eda0e6a12
Ball <piccomed@gmail.com>
2020-08-24 20:36:34 +0700

    Update factory.js

--- ethereum/factory.js
+++ ethereum/factory.js
@@ -1,10 +1,10 @@
 import web3 from './web3';
 import CampaignFactory from './build/CampaignFactory.json';
-import address from '../address.js'
+//import address from '../address.js'
 
 const instance = new web3.eth.Contract(
   JSON.parse(CampaignFactory.interface),
-  address
+  '0x35616B86432d9439D587dAB00043d56d219fac91'
 );
 
 export default instance;
